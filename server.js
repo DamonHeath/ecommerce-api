6eee73580ed243e398441f67c1ff9dba25f8ff4e
unknown <damon.james.heath@gmail.com>
2026-04-21 12:27:33 +0100

    Connect app to database

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const pool = require('./config/db');
 
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('E-Commerce API is running');
+});
+
+app.get('/db-test', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({
+      message: 'Database connection successful',
+      currentTime: result.rows[0]
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'Database connection failed'
+    });
+  }
 });
 
 app.listen(PORT, () => {
